2d48bd4a6b3334f63bea3edbc370eb8d372695fd
OkaPmna <okapramn092@gmail.com>
2026-02-08 00:15:24 +0800

    instant sql import

--- database/unimq.sql
+++ database/unimq.sql
@@ -1,31 +1,34 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1deb3
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Waktu pembuatan: 11 Jan 2026 pada 20.00
--- Versi server: 10.11.13-MariaDB-0ubuntu0.24.04.1
--- Versi PHP: 8.3.6
+-- Hapus database jika sudah ada dan buat baru
+CREATE DATABASE IF NOT EXISTS `unimq`;
+USE `unimq`;
 
+-- Pengaturan Awal
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `unimq`
---
-
+-- --------------------------------------------------------
+-- Struktur Tabel `user`
+-- (Dibuat lebih dulu karena menjadi referensi Foreign Key)
 -- --------------------------------------------------------
 
---
--- Struktur dari tabel `device`
---
+CREATE TABLE `user` (
+  `user_id` int(10) NOT NULL,
+  `user_name` varchar(20) NOT NULL,
+  `password` varchar(225) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+INSERT INTO `user` (`user_id`, `user_name`, `password`) VALUES
+(11, 'rusdingawi', '$2y$10$mAlEsUu84Iio1NRM8RmRnuc/SWCe1Yy5aNimQ28JKrUKeYTHAmiL.');
+
+-- --------------------------------------------------------
+-- Struktur Tabel `device`
+-- --------------------------------------------------------
 
 CREATE TABLE `device` (
   `device_id` int(10) NOT NULL,
@@ -38,75 +41,35 @@ CREATE TABLE `device` (
   `broker_port` varchar(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data untuk tabel `device`
---
-
 INSERT INTO `device` (`device_id`, `broker_url`, `mq_pass`, `mq_user`, `device_type`, `user_id`, `device_name`, `broker_port`) VALUES
 (14, 'test.mosquitto.org', '', '', 'esp32-inkubator', 11, 'Inkubator Ternak 1', '8080'),
 (19, '000000000000000.s1.eu.hivemq.cloud', '54321JJJ', 'cihuyyy', 'esp32-inkubator', 11, 'Inkubator burung', '8884');
 
 -- --------------------------------------------------------
+-- Indeks dan Constraints
+-- --------------------------------------------------------
 
---
--- Struktur dari tabel `user`
---
+-- Indeks untuk tabel `user`
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
 
-CREATE TABLE `user` (
-  `user_id` int(10) NOT NULL,
-  `user_name` varchar(20) NOT NULL,
-  `password` varchar(225) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data untuk tabel `user`
---
-
-INSERT INTO `user` (`user_id`, `user_name`, `password`) VALUES
-(11, 'rusdingawi', '$2y$10$mAlEsUu84Iio1NRM8RmRnuc/SWCe1Yy5aNimQ28JKrUKeYTHAmiL.');
-
---
--- Indexes for dumped tables
---
-
---
 -- Indeks untuk tabel `device`
---
 ALTER TABLE `device`
   ADD PRIMARY KEY (`device_id`),
   ADD KEY `user_id` (`user_id`);
 
---
--- Indeks untuk tabel `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`user_id`);
-
---
--- AUTO_INCREMENT untuk tabel yang dibuang
---
-
---
--- AUTO_INCREMENT untuk tabel `device`
---
-ALTER TABLE `device`
-  MODIFY `device_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
-
---
 -- AUTO_INCREMENT untuk tabel `user`
---
 ALTER TABLE `user`
   MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
---
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
---
+-- AUTO_INCREMENT untuk tabel `device`
+ALTER TABLE `device`
+  MODIFY `device_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
---
--- Ketidakleluasaan untuk tabel `device`
---
+-- Relasi Foreign Key
 ALTER TABLE `device`
   ADD CONSTRAINT `device_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
